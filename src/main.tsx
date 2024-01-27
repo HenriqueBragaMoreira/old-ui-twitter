270b8571e0db49dce1ebc881f4308053e2949c54
HenriqueBragaMoreira <shenrique40moreira@gmail.com>
2024-01-27 15:16:29 -0300

    feat:first commit

---
+++ src/main.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./global.css";
+import { Tweet } from "./components/Tweet";
+import { Sidebar } from "./components/Sidebar";
+import { Header } from "./components/Header";
+import { Separator } from "./components/Separator";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <div className="layout">
+      <Sidebar />
+
+      <div className="content">
+        <main className="timeline">
+          <Header title="Home" />
+
+          <form className="new-tweet-form">
+            <label htmlFor="tweet">
+              <img
+                src="https://github.com/HenriqueBragaMoreira.png"
+                alt="Henrique Braga"
+              />
+              <textarea id="tweet" placeholder="What's happening ?" />
+            </label>
+            <button type="submit">Tweet</button>
+          </form>
+
+          <Separator />
+
+          <Tweet />
+          <Tweet />
+        </main>
+      </div>
+    </div>
+  </React.StrictMode>
+);
